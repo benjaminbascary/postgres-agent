e1291a6fc64b44e63e1fc47cc98d39d87a25c654
benjaminbascary97 <benjamin.bascary@patagoniansys.com>
2023-10-19 21:46:44 -0300

    Fixes table definitions

--- postgres_da_ai_agent/modules/db.py
+++ postgres_da_ai_agent/modules/db.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import sql
 
+
 class PostgresDB:
     def __init__(self):
         self.conn = None
@@ -39,11 +40,13 @@ class PostgresDB:
         self.conn.commit()
 
     def delete(self, table_name, _id):
-        self.cur.execute("DELETE FROM {} WHERE id = %s".format(table_name), (_id,))
+        self.cur.execute(
+            "DELETE FROM {} WHERE id = %s".format(table_name), (_id,))
         self.conn.commit()
 
     def get(self, table_name, _id):
-        self.cur.execute("SELECT * FROM {} WHERE id = %s".format(table_name), (_id,))
+        self.cur.execute(
+            "SELECT * FROM {} WHERE id = %s".format(table_name), (_id,))
         return self.cur.fetchone()
 
     def get_all(self, table_name):
@@ -66,9 +69,51 @@ class PostgresDB:
         return self.cur.fetchone()
 
     def get_all_table_names(self):
-        self.cur.execute("SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
+        self.cur.execute(
+            "SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
         return [row[0] for row in self.cur.fetchall()]
+
+    def get_table_column_definitions(self, table_name):
+        self.cur.execute("""
+            SELECT column_name, data_type, is_nullable, column_default
+            FROM information_schema.columns
+            WHERE table_name = %s
+        """, (table_name,))
+        return self.cur.fetchall()
+
+    def get_foreign_keys(self, table_name):
+        self.cur.execute("""
+            SELECT 
+                kcu.column_name, 
+                ccu.table_name AS foreign_table_name,
+                ccu.column_name AS foreign_column_name
+            FROM 
+                information_schema.table_constraints AS tc 
+                JOIN information_schema.key_column_usage AS kcu
+                    ON tc.constraint_name = kcu.constraint_name
+                JOIN information_schema.constraint_column_usage AS ccu
+                    ON ccu.constraint_name = tc.constraint_name
+            WHERE 
+                tc.constraint_type = 'FOREIGN KEY' AND 
+                tc.table_name = %s;
+        """, (table_name,))
+        return self.cur.fetchall()
 
     def get_table_definitions_for_prompt(self):
         table_names = self.get_all_table_names()
-        return "\n".join([self.get_table_definitions(table_name)[0] for table_name in table_names])
+        result = []
+        for table_name in table_names:
+            result.append(f"Table: {table_name}")
+            columns = self.get_table_column_definitions(table_name)
+            for column in columns:
+                col_name, data_type, is_nullable, col_default = column
+                result.append(
+                    f"    {col_name} ({data_type}), Nullable: {is_nullable}, Default: {col_default}")
+
+            # Add foreign key relationships
+            fks = self.get_foreign_keys(table_name)
+            for fk in fks:
+                col_name, foreign_table_name, foreign_column_name = fk
+                result.append(
+                    f"    FOREIGN KEY ({col_name}) REFERENCES {foreign_table_name}({foreign_column_name})")
+        return "\n".join(result)
