cf013caa0107dee7410eaa36a23d7d0e7b532727
benjaminbascary97 <benjamin.bascary@patagoniansys.com>
2023-10-21 11:59:20 -0300

    Improves prompts

--- postgres_da_ai_agent/main.py
+++ postgres_da_ai_agent/main.py
@@ -85,18 +85,14 @@ def main():
         "A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin." + COMPLETION_PROMPT
     )
     DATA_ENGINEER_PROMPT = (
-        f'''A Data Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
-            Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
-            If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
-            {COMPLETION_PROMPT}
-        '''
+        "A Data Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try." + COMPLETION_PROMPT
     )
     DATA_ANALYST_PROMPT = (
         "A Sr Data Analyst. You follow an approved plan. You run the sql query, generate the response and send it to the product manager for final review." + COMPLETION_PROMPT
     )
 
     PRODUCT_MANAGER_PROMPT = (
-        "A Product Manager. You validate the response to make sure it is correct. You review the response, check carefully if the response fits the desired request from the admin and approve the execution result." + COMPLETION_PROMPT
+        "A Product Manager. You validate the response to make sure it is correct. You review the response, check carefully if the response fits the desired request from the admin and approve the execution result. Finally create a natural language response based on the results of the query that generates de Data Analyst and follow the next instruction: " + COMPLETION_PROMPT
     )
 
     admin_user_proxy_agent = UserProxyAgent(
